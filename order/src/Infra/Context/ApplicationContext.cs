657ac553faef441520713edc54fef04f3eaaf984
FelipeHeilmann <felipeheilmannm@gmail.com>
2024-02-10 17:08:40 -0300

    feat: application context setup

---
+++ order/src/Infra/Context/ApplicationContext.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Customer;
+using Domain.Orders;
+using Domain.Products;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Context;
+
+public class ApplicationContext : DbContext
+{
+    public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+    DbSet<Customer> Customer {  get; set; }
+    DbSet<Category> Category { get; set; }
+    DbSet<Product> Product { get; set; }
+    DbSet<Order> Order { get; set; }
+    DbSet<LineItem> LineItem { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
