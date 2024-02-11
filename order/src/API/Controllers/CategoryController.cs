edb40d75518ae8c4d67fbad07d2df5e15395d5cc
FelipeHeilmann <felipeheilmannm@gmail.com>
2024-02-11 18:49:02 -0300

    feat: category controller get all categories

---
+++ order/src/API/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using Application.Categories.GetAll;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/categories")]
+    [ApiController]
+    public class CategoryController : APIBaseController
+    {
+        public CategoryController(ISender _sender, IHttpContextAccessor _contextAccessor) : base(_sender, _contextAccessor) { }
+
+        [HttpGet]
+        public async Task<IResult> GetAll(CancellationToken cancellationToken)
+        {
+            var query = new GetAllCategoriesQuery();
+
+            var result = await _sender.Send(query);
+
+            return Results.Ok(result.Value);
+        }
+    }
+}
