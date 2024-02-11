69230a124b468a06573ef5bf3eb22cb6a61db817
FelipeHeilmann <felipeheilmannm@gmail.com>
2024-02-11 18:36:48 -0300

    feat: product controller get products

---
+++ order/src/API/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Application.Products.GetAll;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductController : APIBaseController
+    {
+        public ProductController(ISender _sender, IHttpContextAccessor _contextAccessor) : base(_sender, _contextAccessor) { }
+
+        [HttpGet]
+        public async Task<IResult> GetAll(CancellationToken cancellationToken)
+        {
+            var query = new GetAllProductsQuery();
+
+            var result = await _sender.Send(query, cancellationToken);
+
+            return Results.Ok(result.Value);
+        }
+    }
+}
