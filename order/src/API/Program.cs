dde616b93e63f9fdaaf2b485b2342c8752ca80f3
FelipeHeilmann <felipeheilmannm@gmail.com>
2024-02-13 14:49:00 -0300

    feat: masstransist setup

--- order/src/API/Program.cs
+++ order/src/API/Program.cs
@@ -1,5 +1,6 @@
 using API.OptionSetup;
 using Infra;
+using MassTransit;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,6 +18,21 @@ builder.Services.ConfigureOptions<JwtOptionsSetup>();
 builder.Services.ConfigureOptions<JwtBearerOptionsSetup>();
 
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Application.AssemblyReference.Assembly));
+
+builder.Services.AddMassTransit(bussConfigurator =>
+{
+    bussConfigurator.SetKebabCaseEndpointNameFormatter();
+    bussConfigurator.UsingRabbitMq((context, configurator) =>
+    {
+        configurator.Host(new Uri(builder.Configuration["MessageBroker:Host"]!), h =>
+        {
+            h.Username(builder.Configuration["MessageBroker:Username"]);
+            h.Password(builder.Configuration["MessageBroker:Password"]);
+        });
+
+        configurator.ConfigureEndpoints(context);
+    });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ order/src/Domain/Orders/DomainEvents/OrderCheckoutedEvent.cs
@@ -0,0 +1,3 @@
+﻿namespace Domain.Orders.DomainEvents;
+
+public record OrderCheckoutedEvent(Guid OrderId, Guid CustomerId ,DateTime CreateAt, Double Total);
