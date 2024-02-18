6bde0b743721dca83430c400ca8e59c779d8aee5
FelipeHeilmann <felipeheilmannm@gmail.com>
2024-02-18 02:21:04 -0300

    feat: api setup and depency injection - payment

---
+++ payment/src/API/Program.cs
@@ -0,0 +1,40 @@
+using Application.BackgroundTasks;
+using Application.Transactions.Consumers;
+using Infra;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddInfrastructure(builder.Configuration);
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddHostedService<QueueConsumerHostedService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+
+
---
+++ payment/src/Infra/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Application.Abstractions.Queue;
+using Application.Transactions.Consumers;
+using Infra.Queue;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infra;
+
+public static class DependecyInjection
+{
+    public static void AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddSingleton<OrderPurchasedEventConsumer>();
+        services.AddSingleton<IQueue, RabbitMQAdapter>(provider =>
+        {
+            var rabbitMQAdapter = new RabbitMQAdapter(configuration);
+            rabbitMQAdapter.On();
+            return rabbitMQAdapter;
+        });
+    }
+}
