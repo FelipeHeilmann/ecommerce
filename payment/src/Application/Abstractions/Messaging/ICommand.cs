1f9b7835b858092fedb9906e16e7206f69fe804e
FelipeHeilmann <felipeheilmannm@gmail.com>
2024-02-21 23:02:03 -0300

    feat: command and command handler - payment

---
+++ payment/src/Application/Abstractions/Messaging/ICommand.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Shared;
+using MediatR;
+
+namespace Application.Abstractions.Messaging;
+
+public interface ICommand : IRequest<Result>, IBaseCommand
+{
+}
+
+public interface ICommand<TResponse> : IRequest<Result<TResponse>>, IBaseCommand
+{
+}
+
+public interface IBaseCommand
+{
+}
---
+++ payment/src/Application/Abstractions/Messaging/ICommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Shared;
+using MediatR;
+
+namespace Application.Abstractions.Messaging;
+
+public interface ICommandHandler<TCommand> : IRequestHandler<TCommand, Result> where TCommand : ICommand
+{
+    Task<Result> Handle(TCommand command, CancellationToken cancellationToken);
+}
+
+public interface ICommandHandler<TCommand, TResponse> : IRequestHandler<TCommand, Result<TResponse>> where TCommand : ICommand<TResponse>
+{
+    Task<Result<TResponse>> Handle(TCommand command, CancellationToken cancellationToken);
+}
