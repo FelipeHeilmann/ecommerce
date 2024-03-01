ddcd55a62a7c84bda69e7e530d4a284af86533f1
FelipeHeilmann <felipeheilmannm@gmail.com>
2024-02-29 22:02:57 -0300

    feat: application context setup - payment

---
+++ payment/src/Infra/Context/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Refunds;
+using Domain.Transactions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Context;
+
+public class ApplicationContext : DbContext
+{
+    public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+    public DbSet<Transaction> Transaction { get; set; }
+    public DbSet<Refund> Refund { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
+    }
+}
