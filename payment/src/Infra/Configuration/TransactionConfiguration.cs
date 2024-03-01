628e84e8b90667e1b923e8778c4926c9bb1fdbfc
FelipeHeilmann <felipeheilmannm@gmail.com>
2024-02-29 22:04:01 -0300

    feat: transaction ef config

---
+++ payment/src/Infra/Configuration/TransactionConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Transactions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Net;
+namespace Infra.Configuration;
+
+public class TransactionConfiguration : IEntityTypeConfiguration<Transaction>
+{
+    public void Configure(EntityTypeBuilder<Transaction> builder) 
+    {
+        builder.HasKey(t => t.Id);
+        builder.Property(t => t.Id).HasColumnName("id");
+        builder.Property(t => t.OrderId).HasColumnName("order_id");
+        builder.Property(t => t.Amount).HasColumnName("amount");
+        builder.Property(t => t.PaymentServiceId).HasColumnName("payment_service_id");
+        builder.Property(t => t.PaymentType).HasColumnName("payment_type");
+        builder.Property(t => t.Status).HasColumnName("status");
+        builder.Property(t => t.CustomerId).HasColumnName("customer_id");
+        builder.Property(t => t.CreatedAt).HasColumnName("created_at");
+        builder.Property(t => t.ApprovedAt).HasColumnName("approved_at");
+        builder.Property(t => t.RefusedAt).HasColumnName("refused_at");
+
+        builder.ToTable("transactions");
+    }
+}
