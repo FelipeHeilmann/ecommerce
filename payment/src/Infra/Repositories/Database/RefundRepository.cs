ba8f0da21adafe49c99f7bcc0becdbe7a75fe580
FelipeHeilmann <felipeheilmannm@gmail.com>
2024-03-01 23:29:03 -0300

    feat: transaction and refund repository - payment

---
+++ payment/src/Infra/Repositories/Database/RefundRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Refunds;
+using Infra.Context;
+
+namespace Infra.Repositories.Database;
+
+public class RefundRepository : Repository<Refund>, IRefundRepository
+{
+    public RefundRepository(ApplicationContext context) : base(context) { }
+}
---
+++ payment/src/Infra/Repositories/Database/TransactionRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Transactions;
+using Infra.Context;
+
+namespace Infra.Repositories.Database;
+
+public class TransactionRepository : Repository<Transaction> , ITransactionRepository
+{
+    public TransactionRepository(ApplicationContext context) : base(context) { }
+}
