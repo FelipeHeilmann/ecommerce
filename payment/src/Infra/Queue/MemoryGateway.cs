e5b3846370e5688114a517084bba104c379362df
FelipeHeilmann <felipeheilmannm@gmail.com>
2024-03-01 23:34:37 -0300

    feat: unit of work, memory payment gateway

---
+++ payment/src/Infra/Queue/MemoryGateway.cs
@@ -0,0 +1,12 @@
+﻿using Application.Abstractions.Gateway;
+using Application.Transactions.Model;
+
+namespace Infra.Queue;
+
+public class MemoryGateway : IPaymentGateway
+{
+    public Task<PagarmeCreateOrderResponse> CreateOrder(CreateOrderModel request)
+    {
+        return Task.FromResult(new PagarmeCreateOrderResponse(Guid.NewGuid().ToString()));
+    }
+}
