065925fa0f054140112e2a9c93388242c5b2da9f
FelipeHeilmann <felipeheilmannm@gmail.com>
2024-05-01 19:03:11 -0300

    feat: payment recived template

--- nofity/API/Request/Models.cs
+++ nofity/API/Request/Models.cs
@@ -3,4 +3,5 @@
     public record WelcomeRequest(string Email, string Name);
     public record OrderCreatedRequest(Guid OrderId, DateTime Date, string Name, string Email, List<ItemsRequest> Items);
     public record ItemsRequest(string Name, double Price, int Quantity);
+    public record PaymenRecivedRequest(string Name, string Email, Guid OrderId, double Amount);
 }
--- nofity/API/controllers/MailController.cs
+++ nofity/API/controllers/MailController.cs
@@ -45,5 +45,21 @@ namespace API.controllers
 
             return Ok();
         }
+
+        [HttpPost("payment-recived")]
+        public async Task<ActionResult> SendPaymentRecivedMail([FromBody] PaymenRecivedRequest request)
+        {
+            var mailData = new Maildata() 
+            {
+                EmailToEmail = request.Email,
+                EmailToName = request.Name,
+                EmailBody = Templates.PaymentRecived(request.Name, request.OrderId, request.Amount),
+                EmailSubject = "Payment Recived"
+            };
+
+            await _mailerGateway.Send(mailData);
+
+            return Ok();
+        }
     }
 }
