2d530c8cf7fa617536eee603211177c0fd04bfd5
FelipeHeilmann <felipeheilmannm@gmail.com>
2024-05-02 20:37:50 -0300

    feat: add appsetings to.gitignore

--- nofity/API/gateway/MailtrapAdapter.cs
+++ nofity/API/gateway/MailtrapAdapter.cs
@@ -1,41 +1,39 @@
 ﻿using Microsoft.Extensions.Options;
 using MimeKit;
 using MailKit.Net.Smtp;
-using Org.BouncyCastle.Asn1.Pkcs;
 
-namespace API.Gateway
+namespace API.Gateway;
+
+public class MailtrapAdapter : IMailerGateway
 {
-    public class MailtrapAdapter : IMailerGateway
+    private readonly MailtrapSettings _mailSettings;
+    public MailtrapAdapter(IOptions<MailtrapSettings> mailSettingsOptions)
     {
-        private readonly MailtrapSettings _mailSettings;
-        public MailtrapAdapter(IOptions<MailtrapSettings> mailSettingsOptions)
+        _mailSettings = mailSettingsOptions.Value;
+    }
+
+    public async Task Send(Maildata mailData)
+    {      
+      
+        using (MimeMessage emailMessage = new MimeMessage())
         {
-            _mailSettings = mailSettingsOptions.Value;
-        }
+            MailboxAddress emailFrom = new MailboxAddress(_mailSettings.SenderName, _mailSettings.SenderMail);
+            emailMessage.From.Add(emailFrom);
+            emailMessage.From.Add(emailFrom);
+            MailboxAddress emailTo = new MailboxAddress(mailData.EmailToName, mailData.EmailToEmail);
+            emailMessage.To.Add(emailTo);
 
-        public async Task Send(Maildata mailData)
-        {      
-          
-            using (MimeMessage emailMessage = new MimeMessage())
+            BodyBuilder emailBodyBuilder = new BodyBuilder();
+            emailBodyBuilder.HtmlBody = mailData.EmailBody;
+
+            emailMessage.Body = emailBodyBuilder.ToMessageBody();
+            
+            using (SmtpClient mailClient = new SmtpClient())
             {
-                MailboxAddress emailFrom = new MailboxAddress(_mailSettings.SenderName, _mailSettings.SenderMail);
-                emailMessage.From.Add(emailFrom);
-                emailMessage.From.Add(emailFrom);
-                MailboxAddress emailTo = new MailboxAddress(mailData.EmailToName, mailData.EmailToEmail);
-                emailMessage.To.Add(emailTo);
-
-                BodyBuilder emailBodyBuilder = new BodyBuilder();
-                emailBodyBuilder.HtmlBody = mailData.EmailBody;
-
-                emailMessage.Body = emailBodyBuilder.ToMessageBody();
-                
-                using (SmtpClient mailClient = new SmtpClient())
-                {
-                    await mailClient.ConnectAsync(_mailSettings.Host, _mailSettings.Port, MailKit.Security.SecureSocketOptions.StartTls);
-                    await mailClient.AuthenticateAsync(_mailSettings.Username, _mailSettings.Password);
-                    await mailClient.SendAsync(emailMessage);
-                    await mailClient.DisconnectAsync(true);
-                }
+                await mailClient.ConnectAsync(_mailSettings.Host, _mailSettings.Port, MailKit.Security.SecureSocketOptions.StartTls);
+                await mailClient.AuthenticateAsync(_mailSettings.Username, _mailSettings.Password);
+                await mailClient.SendAsync(emailMessage);
+                await mailClient.DisconnectAsync(true);
             }
         }
     }
